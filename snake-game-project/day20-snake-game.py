ba6b163019bdea196068c1f8594ae49eb318ed77
Fred AKLAMANU <fred.aklamanu@ibm.com>
2023-02-21 16:34:26 +0100

    snake project done

--- snake-game-project/day20-snake-game.py
+++ snake-game-project/day20-snake-game.py
@@ -1,6 +1,8 @@
 from turtle import Screen, Turtle
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +12,8 @@ screen.tracer(0)
 
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,9 +25,30 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(0.3)
+    time.sleep(0.1)
     
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
    
+    # Detect collison with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+    #if the heads collides any segment in the tail:
+
 
 screen.exitonclick()
--- snake-game-project/snake.py
+++ snake-game-project/snake.py
@@ -16,13 +16,22 @@ class Snake:
         self.head = self.segments[0]
         
     def create_snake(self):
-         for position in STARTING_POSITION:
-            new_segment = Turtle()
-            new_segment.shape("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+            
+
+    def add_segment(self, position):
+        new_segment = Turtle()
+        new_segment.shape("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+        pass
+
 
     def move(self):
           for seg_num in range(len(self.segments) - 1, 0, -1):
