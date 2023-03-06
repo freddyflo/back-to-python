597632384166df555de4b4b177d1bbb873469722
Fred AKLAMANU <fred.aklamanu@ibm.com>
2023-03-06 09:09:36 +0100

    added high score feature

--- snake-game-project/snake.py
+++ snake-game-project/snake.py
@@ -32,6 +32,12 @@ class Snake:
         self.add_segment(self.segments[-1].position())
         pass
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def move(self):
           for seg_num in range(len(self.segments) - 1, 0, -1):
