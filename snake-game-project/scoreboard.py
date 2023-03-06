a30bd999ccd2c9f0c27d6b59630d0433c8294135
Fred AKLAMANU <fred.aklamanu@ibm.com>
2023-03-06 14:34:29 +0100

    write/read highest score from file

--- snake-game-project/scoreboard.py
+++ snake-game-project/scoreboard.py
@@ -7,7 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_score()
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -21,9 +21,11 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.write_high_score(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
         pass
+    
     # def game_over(self):
     #     self.goto(0,0)
     #     self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
@@ -32,4 +34,14 @@ class Scoreboard(Turtle):
         self.score += 1
         self.update_scoreboard()
         pass
-       
+
+    def read_score(self):
+        with open("data.txt") as file:
+            content = int(file.read())
+            return content
+        pass
+    
+    def write_high_score(self, score):
+        with open("data.txt", mode="w") as file:
+            file.write(str(score))
+        pass
